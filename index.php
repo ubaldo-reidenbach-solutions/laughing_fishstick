a7471dfbebf8eeec504de3cd9f4209acaa7c0323
chomatz <chomatz@gmail.com>
2024-06-18 23:39:46 +0800

    feat: finalize initial web app

--- index.php
+++ index.php
@@ -6,25 +6,49 @@
 	</head>
 	<body>
 		<div class="main">
-			<div class="title"><h1>Demo Web Application</h1></div>
+			<div class="title">
+				<div class="t_box1">Maecenas convallis mattis dolor quis efficitur eros lacinia in sed</div>
+			</div>
 			<ul class="horizontal">
-				<li><a href="#home">Home</a></li>
-				<li><a href="#news">News</a></li>
-				<li><a href="#contact">Contact</a></li>
-				<li><a href="#about">About</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Ultricies</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Egestas</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Donec</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Proin</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Condimentum</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Hendrerit</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Etiam</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Lacinia</a></li>
+				<li class="horizontal"><a class="h_inactive" href="#">Fusce</a></li>
+				<li class="horizontal" style="float:right"><a class="h_active"href="#">Vivamus</a></li>
 			</ul>
 			<ul class="vertical">
-				<li class="vertical"><a class="active" href="#">Item Numnber 0</a></li>
-				<li class="vertical"><a href="#">Item Numnber 1</a></li>
-				<li class="vertical"><a href="#">Item Numnber 2</a></li>
-				<li class="vertical"><a href="#">Item Numnber 3</a></li>
-				<li class="vertical"><a href="#">Item Numnber 4</a></li>
-				<li class="vertical"><a href="#">Item Numnber 5</a></li>
-				<li class="vertical"><a href="#">Item Numnber 6</a></li>
-				<li class="vertical"><a href="#">Item Numnber 7</a></li>
-				<li class="vertical"><a href="#">Item Numnber 8</a></li>
-				<li class="vertical"><a href="#">Item Numnber 9</a></li>
+				<li class="vertical"><a class="v_active" href="#">Aliquam At</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Sed Pretium</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Vestibulum Congue</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Aenean Elementum</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Nunc Erat</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Nam Condimentum</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Mauris Vestibulum</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Orci Varius</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Nulla Rutrum</a></li>
+				<li class="vertical"><a class="v_inactive" href="#">Mauris Sed</a></li>
 			</ul>
+			<div class="window">
+				<div class="w_title">Suspendisse dolor tortor</div>
+				<div class="w_body"><p>Cras finibus urna a est posuere condimentum. Aenean volutpat nec lacus vel ultricies. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In convallis sapien ac massa tristique tempor eu sed lectus. Vivamus sit amet nunc leo. Phasellus augue lorem, condimentum porta mollis ac, fringilla nec ante. Donec quis cursus dolor. Suspendisse sit amet sollicitudin felis. Aliquam interdum erat ut quam elementum, at vehicula orci malesuada. Nullam a ex eros. Proin id lectus vulputate turpis commodo semper in quis urna. Nullam sodales lorem vel ullamcorper rhoncus. Sed a enim euismod ipsum hendrerit dapibus ut vel justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit leo id ante ullamcorper, id porta sem fringilla.</p><p>Pellentesque sed mi id leo interdum elementum. Nulla sit amet quam viverra, commodo arcu vitae, placerat odio. Integer ornare sollicitudin nisi sit amet aliquet. Praesent ligula mi, auctor in eros placerat, auctor tincidunt odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur in facilisis ligula, non vulputate dolor. Fusce arcu tellus, placerat nec varius a, dictum non purus. Sed feugiat sodales ligula a elementum. Integer tincidunt hendrerit ultrices. Etiam sed eleifend tellus. Aenean at felis vulputate, pellentesque est non, gravida lectus. Morbi sed lorem sapien. Donec laoreet dignissim ex a sagittis.</p></div>
+			</div>
+			<div class="window">
+				<div class="w_title">Aenean at lacus neque</div>
+				<div class="w_body"><div class="square1"></div><p>Aenean pretium sapien lacus. Donec sagittis, arcu id dictum ultricies, urna ante scelerisque nisl, vitae tincidunt leo ex vitae augue. Sed commodo mi vehicula enim porttitor imperdiet. Donec non ex vitae felis cursus facilisis. Vivamus consequat tortor non ultricies molestie. Etiam at magna luctus enim accumsan cursus. Praesent mattis lorem id lectus tempor varius. Aliquam nulla turpis, accumsan id dignissim et, rhoncus ut nisl. Nullam luctus consectetur ligula. Vivamus varius diam vitae ipsum euismod consequat sit amet vitae sapien. Proin dignissim ut risus vel lobortis. Donec eu molestie sapien. Suspendisse mattis in nulla sed hendrerit. Sed ac purus et quam aliquet dictum.</p></div>
+			</div>
+			<div class="window">
+				<div class="w_title">Nam sit amet mi est</div>
+				<div class="w_body"><p>Duis quis lacus quis turpis volutpat condimentum vitae non dui. Maecenas eget aliquet quam, vel rhoncus tellus. Vestibulum elementum elementum lacinia. Vestibulum tristique lacus in cursus eleifend. Fusce ut dictum elit. Etiam imperdiet lectus mi, ultrices iaculis tortor fermentum vitae. Pellentesque eget diam quis eros porta sagittis. In lectus dui, aliquam sit amet diam nec, cursus lobortis erat. Morbi congue gravida mauris, in lobortis lacus interdum tempor.</p></div>
+			</div>
+			<div class="window">
+				<div class="w_title">Nunc convallis augue</div>
+				<div class="w_body"><div class="square1"></div><p>Vestibulum et molestie neque. Pellentesque suscipit aliquam fringilla. Etiam semper ipsum eros, nec sollicitudin purus rutrum sit amet. Sed at iaculis urna, ut gravida dolor. Mauris sit amet nibh in magna hendrerit consectetur. Nulla faucibus nisi tellus, vitae dictum est fringilla et. Aenean commodo elit quis dignissim volutpat. Aliquam ligula odio, ultrices id lectus et, facilisis dignissim nulla. Sed auctor dolor et mi iaculis, in rhoncus risus egestas. Sed bibendum auctor velit, et efficitur odio fermentum ac. Vivamus elit dui, sollicitudin in lacinia ac, congue et neque. Morbi venenatis interdum arcu, lacinia interdum ipsum tincidunt et. Nullam ac elementum metus.</p><p>Sed tempor in risus sit amet iaculis. Aliquam erat volutpat. Pellentesque ac ultricies tortor. Ut vitae enim condimentum, laoreet tortor vitae, dignissim metus. Vivamus a metus id lectus ornare consequat eget sit amet justo. Suspendisse potenti. Proin elementum elit at ex tincidunt, a volutpat nunc fermentum.</p></div>
+			</div>
 		</div>
 	</body>
 </html>
