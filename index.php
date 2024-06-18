4c61bd7eeac67d77b1561fd2cbb00c591619a3f1
chomatz <chomatz@gmail.com>
2024-06-18 18:03:01 +0800

    feat: add basic page
    add a basic page and stylesheet

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Demo Web Application</title>
+		<link rel="stylesheet" href="webapp.css">
+	</head>
+	<body>
+		<div class="main">
+			<div class="title"><h1>Demo Web Application</h1></div>
+			<ul class="horizontal">
+				<li><a href="#home">Home</a></li>
+				<li><a href="#news">News</a></li>
+				<li><a href="#contact">Contact</a></li>
+				<li><a href="#about">About</a></li>
+			</ul>
+			<ul class="vertical">
+				<li class="vertical"><a class="active" href="#">Item Numnber 0</a></li>
+				<li class="vertical"><a href="#">Item Numnber 1</a></li>
+				<li class="vertical"><a href="#">Item Numnber 2</a></li>
+				<li class="vertical"><a href="#">Item Numnber 3</a></li>
+				<li class="vertical"><a href="#">Item Numnber 4</a></li>
+				<li class="vertical"><a href="#">Item Numnber 5</a></li>
+				<li class="vertical"><a href="#">Item Numnber 6</a></li>
+				<li class="vertical"><a href="#">Item Numnber 7</a></li>
+				<li class="vertical"><a href="#">Item Numnber 8</a></li>
+				<li class="vertical"><a href="#">Item Numnber 9</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
